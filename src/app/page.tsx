77f39f7086f50d33884261dd78ecf61a9d593237
Hammad <hammadaslam382@gmail.com>
2023-09-26 15:39:00 +0500

    add sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,32 @@
 "use client";
 import Image from "next/image";
 import { CldUploadButton, CldImage } from "next-cloudinary";
+import { useState } from "react";
+type Uploadresult = {
+  info: {
+    public_id: string;
+  };
+  event: "success";
+};
 export default function Home() {
+  const [imageId, setImageId] = useState("");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CldUploadButton uploadPreset="fqeqmrqv" />
-      <CldImage
-        width="960"
-        height="600"
-        src="cld-sample-5"
-        sizes="100vw"
-        alt="Description of my image"
+      <CldUploadButton
+        uploadPreset="fqeqmrqv"
+        onUpload={(result: Uploadresult) => {
+          setImageId(result.info.public_id);
+        }}
       />
+      {imageId && (
+        <CldImage
+          width="400"
+          height="300"
+          src={imageId}
+          sizes="100vw"
+          alt="Description of my image"
+        />
+      )}
     </main>
   );
 }
